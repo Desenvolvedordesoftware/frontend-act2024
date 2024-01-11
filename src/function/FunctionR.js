76dd7bd528153b7d06c1fc2a1d51bcd9cfd53169
Wilson Brito <97698341+Desenvolvedordesoftware@users.noreply.github.com>
2024-01-11 00:18:59 -0300

    up

--- src/function/FunctionR.js
+++ src/function/FunctionR.js
@@ -1,5 +1,5 @@
 export const Codificar = Date.now();
 export const ChaveVenda = Codificar;
 
-export const codCompany = "31784981000139"
-export const url = "https://acthauros.com.br/api"
+export const codCompany = "31784981000139";
+export const url = "https://acthauros.com.br/api";
