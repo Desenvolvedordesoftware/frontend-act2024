eacf3838e0c2edd1c226080e5ff9c4881b23a8e9
Wilson Brito <97698341+Desenvolvedordesoftware@users.noreply.github.com>
2024-01-16 01:09:40 -0300

    UP

--- src/pages/Stock/Products/ListProducts.js
+++ src/pages/Stock/Products/ListProducts.js
@@ -1,12 +1,16 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import styled from "styled-components";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
 import {
   Button,
 } from '@mui/material';
 import AssignmentIndIcon from '@mui/icons-material/AssignmentInd';
 import Modal from '@mui/material/Modal';
 import DataTable from 'react-data-table-component';
+import { url } from '../../../function/FunctionR';
 
 const style = {
   position: 'absolute',
@@ -35,9 +39,30 @@ export const Div = styled.div`
 
 
 const ListProducts = () => {
+  const usersStorage = JSON.parse(localStorage.getItem("users_db"));
+  const CodCompany = usersStorage.map((user) => user.CodCompany);
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+  const [box, setBox] = React.useState([]);
+  const [records, setRecords] = React.useState(box);
+
+  const getBox = async () => {
+    
+
+    try {
+        const res = await axios.get(url + "/stock/productslist/" + CodCompany);
+       
+        if (res.status === 200) {
+          setBox(res.data.stmt);
+        } else {
+          setBox([])
+          return toast.warn("Não foi encotrado movimentações!");
+        }
+    } catch (error) {
+        toast.error(error);
+    }
+  }
 
   const columns = [
     {
@@ -56,292 +81,13 @@ const ListProducts = () => {
       sortable: true
     }
   ];
-  const data = [
-    {
-      id: "1",
-      CODIGO: "1",
-      PRODUTO: "RUFFLES CEB&SALSA 57G",
-      PRECOVENDA: "45",
-      ESTOQUE: "255"
-    },
-    {
-      id: "2",
-      CODIGO: "2",
-      PRODUTO: "BIS XTRA 45G",
-      PRECOVENDA: "3.45",
-      ESTOQUE: "122"
-    },
-    {
-      id: "3",
-      CODIGO: "3",
-      PRODUTO: "BIS XTRA BLACK 45G",
-      PRECOVENDA: "5",
-      ESTOQUE: "55"
-    },
-    {
-      id: "4",
-      CODIGO: "4",
-      PRODUTO: "CHOCOLATE LACTA LAKE 80G",
-      PRECOVENDA: "12",
-      ESTOQUE: "87"
-    },
-    {
-      id: "5",
-      CODIGO: "5",
-      PRODUTO: "BARRA DE CHOCOLATE E LEITE LACTA 90G",
-      PRECOVENDA: "17.56",
-      ESTOQUE: "67"
-    },
-    {
-      id: "6",
-      CODIGO: "6",
-      PRODUTO: "CHEETOS ASSADO SAB. REQUEIJAO 45G",
-      PRECOVENDA: "12",
-      ESTOQUE: "45"
-    },
-    {
-      id: "7",
-      CODIGO: "7",
-      PRODUTO: "CHOCOLATE LAKA DIAMENTE NEGRO 90G",
-      PRECOVENDA: "5",
-      ESTOQUE: "31"
-    },
-    {
-      id: "8",
-      CODIGO: "8",
-      PRODUTO: "CHOCOLATE DIAMENTE NEGRO 80G",
-      PRECOVENDA: "5",
-      ESTOQUE: "123"
-    },
-    {
-      id: "10",
-      CODIGO: "10",
-      PRODUTO: "AMENDOIM TIPO JAPONES DORI 70G",
-      PRECOVENDA: "5",
-      ESTOQUE: "123"
-    },
-    {
-      id: "11",
-      CODIGO: "11",
-      PRODUTO: "AMENDOIM PETTIZ NATURAL CROCANTE 50G",
-      PRECOVENDA: "5",
-      ESTOQUE: "110"
-    },
-    {
-      id: "12",
-      CODIGO: "12",
-      PRODUTO: "AMENDOIM PETTIZ SAB. CEBOLA E SALSA 50G",
-      PRECOVENDA: "5",
-      ESTOQUE: "11"
-    },
-    {
-      id: "13",
-      CODIGO: "13",
-      PRODUTO: "AMENDOIM PETTIZ SAB. PIMENTA VERMELHA 50G",
-      PRECOVENDA: "59",
-      ESTOQUE: "18"
-    },
-    {
-      id: "14",
-      CODIGO: "14",
-      PRODUTO: "CHOCOLATE LACTA SHOT 90G",
-      PRECOVENDA: "57",
-      ESTOQUE: "34"
-    },
-    {
-      id: "15",
-      CODIGO: "15",
-      PRODUTO: "KIT KAT 41,5G",
-      PRECOVENDA: "65",
-      ESTOQUE: "87"
-    },
-    {
-      id: "16",
-      CODIGO: "16",
-      PRODUTO: "DORITOS QUEIJO NACHO 45G",
-      PRECOVENDA: "51",
-      ESTOQUE: "78"
-    },
-    {
-      id: "17",
-      CODIGO: "17",
-      PRODUTO: "BATATAS MAX 40G",
-      PRECOVENDA: "53",
-      ESTOQUE: "65"
-    },
-    {
-      id: "18",
-      CODIGO: "18",
-      PRODUTO: "CEBOLITOS ELMA CHIPS 60G",
-      PRECOVENDA: "55",
-      ESTOQUE: "55"
-    },
-    {
-      id: "19",
-      CODIGO: "19",
-      PRODUTO: "RUFFLES ORIGINAL SAL 76G",
-      PRECOVENDA: "25",
-      ESTOQUE: "33"
-    },
-    {
-      id: "20",
-      CODIGO: "20",
-      PRODUTO: "RUFFLES CHURRASCO 57G",
-      PRECOVENDA: "35",
-      ESTOQUE: "13"
-    },
-    {
-      id: "21",
-      CODIGO: "21",
-      PRODUTO: "STAX CHEDDAR 156G",
-      PRECOVENDA: "25",
-      ESTOQUE: "12"
-    },
-    {
-      id: "1",
-      CODIGO: "1",
-      PRODUTO: "RUFFLES CEB&SALSA 57G",
-      PRECOVENDA: "45",
-      ESTOQUE: "255"
-    },
-    {
-      id: "2",
-      CODIGO: "2",
-      PRODUTO: "BIS XTRA 45G",
-      PRECOVENDA: "3.45",
-      ESTOQUE: "122"
-    },
-    {
-      id: "3",
-      CODIGO: "3",
-      PRODUTO: "BIS XTRA BLACK 45G",
-      PRECOVENDA: "5",
-      ESTOQUE: "55"
-    },
-    {
-      id: "4",
-      CODIGO: "4",
-      PRODUTO: "CHOCOLATE LACTA LAKE 80G",
-      PRECOVENDA: "12",
-      ESTOQUE: "87"
-    },
-    {
-      id: "5",
-      CODIGO: "5",
-      PRODUTO: "BARRA DE CHOCOLATE E LEITE LACTA 90G",
-      PRECOVENDA: "17.56",
-      ESTOQUE: "67"
-    },
-    {
-      id: "6",
-      CODIGO: "6",
-      PRODUTO: "CHEETOS ASSADO SAB. REQUEIJAO 45G",
-      PRECOVENDA: "12",
-      ESTOQUE: "45"
-    },
-    {
-      id: "7",
-      CODIGO: "7",
-      PRODUTO: "CHOCOLATE LAKA DIAMENTE NEGRO 90G",
-      PRECOVENDA: "5",
-      ESTOQUE: "31"
-    },
-    {
-      id: "8",
-      CODIGO: "8",
-      PRODUTO: "CHOCOLATE DIAMENTE NEGRO 80G",
-      PRECOVENDA: "5",
-      ESTOQUE: "123"
-    },
-    {
-      id: "10",
-      CODIGO: "10",
-      PRODUTO: "AMENDOIM TIPO JAPONES DORI 70G",
-      PRECOVENDA: "5",
-      ESTOQUE: "123"
-    },
-    {
-      id: "11",
-      CODIGO: "11",
-      PRODUTO: "AMENDOIM PETTIZ NATURAL CROCANTE 50G",
-      PRECOVENDA: "5",
-      ESTOQUE: "110"
-    },
-    {
-      id: "12",
-      CODIGO: "12",
-      PRODUTO: "AMENDOIM PETTIZ SAB. CEBOLA E SALSA 50G",
-      PRECOVENDA: "5",
-      ESTOQUE: "11"
-    },
-    {
-      id: "13",
-      CODIGO: "13",
-      PRODUTO: "AMENDOIM PETTIZ SAB. PIMENTA VERMELHA 50G",
-      PRECOVENDA: "59",
-      ESTOQUE: "18"
-    },
-    {
-      id: "14",
-      CODIGO: "14",
-      PRODUTO: "CHOCOLATE LACTA SHOT 90G",
-      PRECOVENDA: "57",
-      ESTOQUE: "34"
-    },
-    {
-      id: "15",
-      CODIGO: "15",
-      PRODUTO: "KIT KAT 41,5G",
-      PRECOVENDA: "65",
-      ESTOQUE: "87"
-    },
-    {
-      id: "16",
-      CODIGO: "16",
-      PRODUTO: "DORITOS QUEIJO NACHO 45G",
-      PRECOVENDA: "51",
-      ESTOQUE: "78"
-    },
-    {
-      id: "17",
-      CODIGO: "17",
-      PRODUTO: "BATATAS MAX 40G",
-      PRECOVENDA: "53",
-      ESTOQUE: "65"
-    },
-    {
-      id: "18",
-      CODIGO: "18",
-      PRODUTO: "CEBOLITOS ELMA CHIPS 60G",
-      PRECOVENDA: "55",
-      ESTOQUE: "55"
-    },
-    {
-      id: "19",
-      CODIGO: "19",
-      PRODUTO: "RUFFLES ORIGINAL SAL 76G",
-      PRECOVENDA: "25",
-      ESTOQUE: "33"
-    },
-    {
-      id: "20",
-      CODIGO: "20",
-      PRODUTO: "RUFFLES CHURRASCO 57G",
-      PRECOVENDA: "35",
-      ESTOQUE: "13"
-    },
-    {
-      id: "21",
-      CODIGO: "21",
-      PRODUTO: "STAX CHEDDAR 156G",
-      PRECOVENDA: "25",
-      ESTOQUE: "12"
-    },
-  ]
+  React.useEffect(() => {
+    getBox();
+    return;
+}, []);
 
-  const [records, setRecords] = React.useState(data);
   function handleFilter(event) {
-    const newData = data.filter(row => {
+    const newData = box.filter(row => {
       return row.PRODUTO.toLowerCase().includes(event.target.value.toLowerCase())
     })
     setRecords(newData)
